b7def189a9759c74b7700cb1fee68d88546657ce
braian <brasanluc123@gmail.com>
2023-06-23 20:30:25 -0300

    Se agrega el proyecto

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{
+      flex: 1,
+      backgroundColor: "#f2f2f2",
+      justifyContent: "center"
+
+    }}>
+
+      <Text style={{
+        fontSize: 25,
+        textAlign: 'center'
+      }}> Hola Mundo !!</Text>
+
+    </View >
+  )
+}
+
+
+
+export default App;
