9bf42e25a0617f0d62da926a4c323642383e41e6
braian <brasanluc123@gmail.com>
2023-06-25 05:21:45 -0300

    Flexible Positioning

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import { SafeAreaView } from 'react-native';
 // SafeAreaView es mas que nada para ios para que no choque con el noch 
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ height: 400 }}>
       {/* // <ContadorScreen /> */}
       <BoxObjectModelScreen />
 
--- src/screens/BoxObjectModelScreen.tsx
+++ src/screens/BoxObjectModelScreen.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export const BoxObjectModelScreen = () => {
     return (
-        <View>
-
-            <Text>
-                BoxObjectModel
-            </Text>
-
+        <View style={styles.container}>
+            <Text style={styles.title}>BoxObjectModel</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'orange',
+        justifyContent: 'center',
+
+    },
+    title: {
+        padding: 10,
+        fontSize: 30,
+        borderWidth: 9,
+        textAlign: 'center',
+        // alignSelf: 'center',
+        marginRight: "auto",
+        marginLeft: "auto",
+
+    }
+});
