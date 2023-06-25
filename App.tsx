310632ea6f384e862958d0a5e03f25eb264e9bb6
braian <brasanluc123@gmail.com>
2023-06-25 04:34:51 -0300

    Box object model screen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { ContadorScreen } from './src/screens/ContadorScreen';
+// import { ContadorScreen } from './src/screens/ContadorScreen';
+import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
+import { SafeAreaView } from 'react-native';
 
+// SafeAreaView es mas que nada para ios para que no choque con el noch 
 const App = () => {
   return (
-    <ContadorScreen />
+    <SafeAreaView>
+      {/* // <ContadorScreen /> */}
+      <BoxObjectModelScreen />
+
+    </SafeAreaView>
   );
 };
 export default App;
---
+++ src/screens/BoxObjectModelScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+export const BoxObjectModelScreen = () => {
+    return (
+        <View>
+
+            <Text>
+                BoxObjectModel
+            </Text>
+
+        </View>
+    )
+}
