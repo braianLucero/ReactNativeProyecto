9a58659b49c00eaa927b2d9862281be9becb39bb
braian <brasanluc123@gmail.com>
2023-06-26 17:33:03 -0300

    TP 3 resolved (giving flexibility to the boxes)

--- src/screens/TareaScreen.tsx
+++ src/screens/TareaScreen.tsx
@@ -15,14 +15,16 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#284258',
+        justifyContent: 'center'
     },
     cajaMorada: {
         width: 100,
-
         height: 100,
         borderWidth: 10,
         borderColor: 'white',
-        backgroundColor: '#5856D6'
+        backgroundColor: '#5856D6',
+        marginLeft: 'auto'
+
     },
     cajaNaranja: {
         width: 100,
@@ -32,10 +34,14 @@ const styles = StyleSheet.create({
         backgroundColor: '#F0A23B'
     },
     cajaAzul: {
-        width: 500,
+        width: 100,
         height: 100,
         borderWidth: 10,
         borderColor: 'white',
-        backgroundColor: '#28C4D9'
+        backgroundColor: '#28C4D9',
+        marginLeft: 'auto',
+        marginRight: 'auto'
+
+
     },
 });
