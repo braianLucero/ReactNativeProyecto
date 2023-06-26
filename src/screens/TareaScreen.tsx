4e6f6ad554e3485a677433f18f97fae5cd92d1ef
braian <brasanluc123@gmail.com>
2023-06-26 17:51:31 -0300

    TP 6 resolved (differentiating box flexibility)

--- src/screens/TareaScreen.tsx
+++ src/screens/TareaScreen.tsx
@@ -15,28 +15,29 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#284258',
-        flexDirection: 'row',
-        justifyContent: 'space-between'
+        // flexDirection: 'row',
 
     },
     cajaMorada: {
-        width: 100,
-        height: 900,
+        flex: 1,
+        width: 410,
+        height: 100,
         borderWidth: 10,
         borderColor: 'white',
-        backgroundColor: '#5856D6',
-
+        backgroundColor: '#5856D6'
     },
     cajaNaranja: {
-        width: 100,
-        height: 900,
+        flex: 1,
+        width: 410,
+        height: 100,
         borderWidth: 10,
         borderColor: 'white',
         backgroundColor: '#F0A23B',
     },
     cajaAzul: {
-        width: 100,
-        height: 900,
+        flex: 2,
+        width: 410,
+        height: 100,
         borderWidth: 10,
         borderColor: 'white',
         backgroundColor: '#28C4D9',
