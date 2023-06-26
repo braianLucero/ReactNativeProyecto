f506de79abd8b3b7f84bbb8dd8bddc6b01cfdcbd
braian <brasanluc123@gmail.com>
2023-06-26 17:47:31 -0300

    Tp 5 resolved (expanding box positions)

--- src/screens/TareaScreen.tsx
+++ src/screens/TareaScreen.tsx
@@ -15,37 +15,31 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#284258',
-        justifyContent: 'center'
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+
     },
     cajaMorada: {
         width: 100,
-        height: 100,
+        height: 900,
         borderWidth: 10,
         borderColor: 'white',
         backgroundColor: '#5856D6',
-        position: 'absolute',
-        top: 0,
-        right: 0
 
     },
     cajaNaranja: {
         width: 100,
-        height: 100,
+        height: 900,
         borderWidth: 10,
         borderColor: 'white',
         backgroundColor: '#F0A23B',
-        marginLeft: 'auto',
-        marginRight: 'auto'
     },
     cajaAzul: {
         width: 100,
-        height: 100,
+        height: 900,
         borderWidth: 10,
         borderColor: 'white',
         backgroundColor: '#28C4D9',
-        position: 'absolute',
-        left: 0,
-        bottom: 0
 
 
 
