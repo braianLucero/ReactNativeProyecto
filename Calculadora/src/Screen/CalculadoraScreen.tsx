896f1c052b3873090e6fa8b6f710984f9450ae26
braian <brasanluc123@gmail.com>
2023-06-27 15:20:20 -0300

    initial design for a calculator with a simple and professional look

---
+++ Calculadora/src/Screen/CalculadoraScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+import { styles } from '../theme/appThem';
+
+export const CalculadoraScreen = () => {
+    return (
+        <View>
+            <Text style={styles.texto}>Calculadora </Text>
+        </View>
+    )
+}
