01394ce8d7aa2b7fca5b00effff684ffe32248fb
braian <brasanluc123@gmail.com>
2023-06-29 23:36:07 -0300

    building side menu

--- NavegacionApp/App.tsx
+++ NavegacionApp/App.tsx
@@ -1,13 +1,15 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { StackNavigator } from './src/Navigator/StackNavigator';
+// import { StackNavigator } from './src/Navigator/StackNavigator';
+import { MenuLateralBasico } from './src/Navigator/MenuLateralBasico';
 
 const App = () => {
   return (
     <NavigationContainer>
 
-      <StackNavigator />
+      {/* <StackNavigator /> */}
+      <MenuLateralBasico />
 
     </NavigationContainer>
   )
