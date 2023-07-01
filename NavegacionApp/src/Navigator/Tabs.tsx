d92ec87f975b2550ffd3a2f7024d44a8ce99a820
braian <brasanluc123@gmail.com>
2023-06-30 23:56:27 -0300

    Create component screen Tabs

---
+++ NavegacionApp/src/Navigator/Tabs.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+export const Tabs = () => {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+    );
+}
---
+++ NavegacionApp/src/Screen/Tabs1Screen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+export const Tabs1Screen = () => {
+    return (
+        <View>
+            <Text>Tab 1 Screen </Text>
+        </View>
+    )
+}
