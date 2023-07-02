bbabf835f22cef14423c05867e77aad2e54eaea2
braian <brasanluc123@gmail.com>
2023-07-02 02:15:22 -0300

    creating my navigation stack

---
+++ LoAprendido/src/Screens/PantallaScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+export const PantallaScreen = () => {
+    return (
+        <View>
+
+            <Text> Hola estoy practicando lo aprendido </Text>
+
+        </View>
+    )
+}
