cdfec75030f10f990ffade81a83682a027495c2d
braian <brasanluc123@gmail.com>
2023-07-02 19:55:01 -0300

    customizing the Drawer Navigation

--- LoAprendido/src/Navegaciones/DrawerNavigation.tsx
+++ LoAprendido/src/Navegaciones/DrawerNavigation.tsx
@@ -1,15 +1,48 @@
 import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItem, DrawerContentComponentProps, DrawerContentOptions } from '@react-navigation/drawer';
 import { StackNavigation } from './StackNavigation';
 import { DrawersScreen } from '../Screens/Screens/Drawers/DrawersScreen';
+import { View, Image } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 export const DrawerNavigation = () => {
     return (
-        <Drawer.Navigator>
+        <Drawer.Navigator
+            drawerContent={(Props) => <CustomDrawerContent {...Props} />}
+        >
             <Drawer.Screen name="StackNavigation" component={StackNavigation} />
             <Drawer.Screen name="DrawerScreen" component={DrawersScreen} />
         </Drawer.Navigator>
     );
 }
+
+
+const CustomDrawerContent = ({ navigation }: DrawerContentComponentProps<DrawerContentOptions>) => {
+    return (
+        <View style={{ flex: 1 }}>
+            <DrawerContentScrollView>
+                {/* Agrega aquí tu imagen */}
+                <View style={{ alignItems: 'center', marginTop: 20 }}>
+                    <Image
+                        source={{
+                            uri: 'https://st3.depositphotos.com/9998432/13335/v/600/depositphotos_133352010-stock-illustration-default-placeholder-man-and-woman.jpg'
+                        }}
+                        style={{ width: 150, height: 150, borderRadius: 75 }}
+                    />
+                </View>
+
+                {/* Resto de los elementos del menú lateral */}
+                <DrawerItem
+                    label="StackNavigation"
+                    onPress={() => navigation.navigate('StackNavigation')}
+                />
+                <DrawerItem
+                    label="DrawerScreen"
+                    onPress={() => navigation.navigate('DrawerScreen')}
+                />
+                {/* ...otros elementos del menú */}
+            </DrawerContentScrollView>
+        </View>
+    );
+};
