ebe0618b8792e65de7d41c19166f1a469ae0c5f0
braian <brasanluc123@gmail.com>
2023-07-02 23:49:48 -0300

    Customizing the tabs

--- LoAprendido/src/Navegaciones/TabsNavigation.tsx
+++ LoAprendido/src/Navegaciones/TabsNavigation.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { TabsScreen } from '../Screens/Screens/Tabs/TabsScreen';
+import { StyleSheet } from 'react-native';
 import { Tabs2Screen } from '../Screens/Screens/Tabs/Tabs2Screen ';
 import { Tabs3Screen } from '../Screens/Screens/Tabs/Tabs3Screen ';
 
@@ -8,10 +9,29 @@ const Tab = createBottomTabNavigator();
 
 export const Tabs = () => {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            tabBarOptions={{
+                activeTintColor: '#000',
+                inactiveTintColor: '#999999',
+                style: styles.tabBar,
+                labelStyle: styles.tabLabel,
+            }}
+        >
             <Tab.Screen name="TabsScreen" component={TabsScreen} />
             <Tab.Screen name="Tabs2Screen" component={Tabs2Screen} />
             <Tab.Screen name="Tabs3Screen" component={Tabs3Screen} />
         </Tab.Navigator>
     );
 }
+
+const styles = StyleSheet.create({
+    tabBar: {
+        backgroundColor: '#333333',
+        borderTopWidth: 1,
+        borderTopColor: '#a8a8a8',
+    },
+    tabLabel: {
+        fontSize: 14,
+        fontWeight: 'bold',
+    },
+});
