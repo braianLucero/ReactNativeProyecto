520ec8f303cef8b4f1a17ee3a7ad537187abcff6
braian <brasanluc123@gmail.com>
2023-07-04 23:37:49 -0300

    Creating Inputs

--- MiniProyecto/src/Screens/LoginScreen.tsx
+++ MiniProyecto/src/Screens/LoginScreen.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
 export const LoginScreen = () => {
     return (
         <View style={styles.Container}>
 
-            <Text style={styles.text}>Login</Text>
+            <Text style={styles.text}>Hello</Text>
+            <Text style={styles.subTitle}>Sig In to your account</Text>
+            <TextInput
+                placeholder='Brasanluc123@gmail.com'
+                style={styles.textInput}
+            />
+
+            <TextInput
+                placeholder='password'
+                style={styles.textInput}
+
+            />
 
         </View>
     )
@@ -18,6 +29,19 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     text: {
-        fontSize: 30
+        fontSize: 30,
+        color: '#000',
+        fontWeight: 'bold'
+
+    },
+    subTitle: {
+        fontSize: 20,
+        color: 'gray',
+
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: 'gray'
     }
+
 });
