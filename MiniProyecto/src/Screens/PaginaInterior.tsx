53c5fff1a388bb1bcc58e94493dcf984e327aced
braian <brasanluc123@gmail.com>
2023-07-11 13:13:28 -0300

    src/Screens/PaginaInterior.tsx

---
+++ MiniProyecto/src/Screens/PaginaInterior.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+export const PaginaInterior = () => {
+    return (
+        <View>
+            <Text>Pagina Interior</Text>
+        </View>
+    )
+}
