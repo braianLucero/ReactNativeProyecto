820100906b7a322764877b793ccab12ebe4b1ee3
braian <brasanluc123@gmail.com>
2023-07-12 13:30:01 -0300

    Update App component and dependencies

--- MiniProyecto/App.tsx
+++ MiniProyecto/App.tsx
@@ -2,17 +2,17 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import { StackNavigation } from './src/Navigations/StackNavigation';
-import Carousel from './src/Components/Carousel';
+// import Carousel from './src/Components/Carousel';
 
 export const App = () => {
   return (
-    // <>
-    //   <StatusBar barStyle="dark-content" />
-    //   <SafeAreaView style={{ flex: 1 }}>
-    //     <StackNavigation />
-    //   </SafeAreaView>
-    // </>
-    <Carousel />
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <StackNavigation />
+      </SafeAreaView>
+    </>
+    // <Carousel />
   );
 };
 
