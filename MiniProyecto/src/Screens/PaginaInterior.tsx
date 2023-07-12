edfea8d045ec7f82c05e26b12c1ef3e3edae0e4c
braian <brasanluc123@gmail.com>
2023-07-11 23:44:01 -0300

    Refactor PaginaInterior component for better organization

--- MiniProyecto/src/Screens/PaginaInterior.tsx
+++ MiniProyecto/src/Screens/PaginaInterior.tsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, Image, Button } from 'react-native';
 
 export const PaginaInterior = () => {
     return (
-        <View>
-            <Text>Pagina Interior</Text>
+        <View style={styles.container}>
+            <Image
+                source={require('../assets/image.png')}
+                style={styles.image}
+            />
+            <Text style={styles.title}>Pagina Interior</Text>
+            <Text style={styles.description}>
+                Bienvenido a la página interior de la aplicación.
+            </Text>
+            <Button
+                title="Realizar acción"
+                onPress={() => {
+                    // Acción a realizar que aun estoy pensando 
+                }}
+            />
         </View>
-    )
-}
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        width: 200,
+        height: 200,
+        marginBottom: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    description: {
+        fontSize: 16,
+        textAlign: 'center',
+        marginBottom: 20,
+    },
+});
+
+// tengo que crearme una carpeta Theme para poder exportar los estilos de PaginaINterna 
