0cf71a7f8826433596db23204fa95d5b26f4d50d
braian <brasanluc123@gmail.com>
2023-07-12 13:30:55 -0300

    Components/Carousel

--- MiniProyecto/src/Components/Carousel.tsx
+++ MiniProyecto/src/Components/Carousel.tsx
@@ -1,73 +1,73 @@
-import React from 'react';
-import { View, Image, StyleSheet, ScrollView, Dimensions, FlatList } from 'react-native';
+// import React from 'react';
+// import { View, Image, StyleSheet, ScrollView, Dimensions, FlatList } from 'react-native';
 
-const Carousel = ({ images }) => {
-    const renderItem = ({ item }) => (
-        <View style={styles.imageContainer}>
-            <Image source={item.source} style={styles.image} />
-        </View>
-    );
+// const Carousel = ({ images }) => {
+//     const renderItem = ({ item }) => (
+//         <View style={styles.imageContainer}>
+//             <Image source={item.source} style={styles.image} />
+//         </View>
+//     );
 
-    return (
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            <FlatList
-                data={images}
-                renderItem={renderItem}
-                keyExtractor={(item) => item.id.toString()}
-                horizontal
-                snapToInterval={Dimensions.get('window').width}
-                decelerationRate="fast"
-            />
-        </ScrollView>
-    );
-};
+//     return (
+//         <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+//             <FlatList
+//                 data={images}
+//                 renderItem={renderItem}
+//                 keyExtractor={(item) => item.id.toString()}
+//                 horizontal
+//                 snapToInterval={Dimensions.get('window').width}
+//                 decelerationRate="fast"
+//             />
+//         </ScrollView>
+//     );
+// };
 
-const styles = StyleSheet.create({
-    imageContainer: {
-        width: Dimensions.get('window').width,
-        alignItems: 'center',
-    },
-    image: {
-        width: '95%',
-        height: 200,
-        resizeMode: 'cover',
-        marginTop: 19,
-        borderRadius: 20,
-    },
-});
-
-const CarouselContainer = () => {
-    const images = [
-        { id: 1, source: require('../img/img1.webp') },
-        { id: 2, source: require('../img/img2.webp') },
-        { id: 3, source: require('../img/img3.webp') },
-        { id: 4, source: require('../img/img4.webp') },
-        { id: 5, source: require('../img/img5.webp') },
-    ];
-
-    return (
-        <ScrollView>
-            <View style={carouselStyles.carouselContainer}>
-                <Carousel images={images} />
-                <Carousel images={images} />
-                <Carousel images={images} />
-                <Carousel images={images} />
-                <Carousel images={images} />
-            </View>
-        </ScrollView>
-    );
-};
-
-const carouselStyles = StyleSheet.create({
-    carouselContainer: {
-        paddingBottom: 20,
-    },
-});
 // const styles = StyleSheet.create({
+//     imageContainer: {
+//         width: Dimensions.get('window').width,
+//         alignItems: 'center',
+//     },
+//     image: {
+//         width: '95%',
+//         height: 200,
+//         resizeMode: 'cover',
+//         marginTop: 19,
+//         borderRadius: 20,
+//     },
+// });
+
+// const CarouselContainer = () => {
+//     const images = [
+//         { id: 1, source: require('../img/img1.webp') },
+//         { id: 2, source: require('../img/img2.webp') },
+//         { id: 3, source: require('../img/img3.webp') },
+//         { id: 4, source: require('../img/img4.webp') },
+//         { id: 5, source: require('../img/img5.webp') },
+//     ];
+
+//     return (
+//         <ScrollView>
+//             <View style={carouselStyles.carouselContainer}>
+//                 <Carousel images={images} />
+//                 <Carousel images={images} />
+//                 <Carousel images={images} />
+//                 <Carousel images={images} />
+//                 <Carousel images={images} />
+//             </View>
+//         </ScrollView>
+//     );
+// };
+
+// const carouselStyles = StyleSheet.create({
 //     carouselContainer: {
 //         paddingBottom: 20,
 //     },
 // });
+// // const styles = StyleSheet.create({
+// //     carouselContainer: {
+// //         paddingBottom: 20,
+// //     },
+// // });
 
-export default CarouselContainer;
+// export default CarouselContainer;
 
