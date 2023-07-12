f306999f3af7cc92389af3ae36f9d9fa97e7e307
braian <brasanluc123@gmail.com>
2023-07-12 11:25:49 -0300

    Incorporation of vertical scrolling to display multiple images

--- MiniProyecto/src/Screens/PaginaInterior.tsx
+++ MiniProyecto/src/Screens/PaginaInterior.tsx
@@ -1,48 +1,47 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, Button } from 'react-native';
+import { View, Image, StyleSheet, ScrollView } from 'react-native';
 
 export const PaginaInterior = () => {
     return (
-        <View style={styles.container}>
+        // ScrollView  se creará un área de desplazamiento que permitirá desplazarse hacia abajo si el contenido excede el espacio disponible en la pantalla.
+        <ScrollView contentContainerStyle={styles.container}>
             <Image
-                source={require('../assets/image.png')}
+                source={require('../img/img1.webp')}
                 style={styles.image}
             />
-            <Text style={styles.title}>Pagina Interior</Text>
-            <Text style={styles.description}>
-                Bienvenido a la página interior de la aplicación.
-            </Text>
-            <Button
-                title="Realizar acción"
-                onPress={() => {
-                    // Acción a realizar que aun estoy pensando 
-                }}
+            <Image
+                source={require('../img/img2.webp')}
+                style={styles.image}
             />
-        </View>
+            <Image
+                source={require('../img/img3.webp')}
+                style={styles.image}
+            />
+            <Image
+                source={require('../img/img4.webp')}
+                style={styles.image}
+            />
+            <Image
+                source={require('../img/img5.webp')}
+                style={styles.image}
+            />
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flexGrow: 1,
         alignItems: 'center',
         justifyContent: 'center',
     },
     image: {
         width: 200,
         height: 200,
-        marginBottom: 20,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
         marginBottom: 10,
-    },
-    description: {
-        fontSize: 16,
-        textAlign: 'center',
-        marginBottom: 20,
     },
 });
 
-// tengo que crearme una carpeta Theme para poder exportar los estilos de PaginaINterna 
+
+
+// tengo que crearme una carpeta Theme para poder exportar los estilos de PaginaINterna
