40f508014268e2e36473309087761cc7d1185a84
braian <brasanluc123@gmail.com>
2023-07-13 23:57:42 -0300

    Adding authentication functionality

--- MiniProyecto/src/Navigations/DrawerNavigation.tsx
+++ MiniProyecto/src/Navigations/DrawerNavigation.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { StackNavigation } from './StackNavigation';
+import { HomeScreen } from './HomeScreen';
+import { SettingsScreen } from './SettingsScreen';
 
 const Drawer = createDrawerNavigator();
 
 export const DrawerNavigation = () => {
     return (
         <Drawer.Navigator>
+            <Drawer.Screen name="Home" component={HomeScreen} />
             <Drawer.Screen name="StackNavigation" component={StackNavigation} />
+            <Drawer.Screen name="Settings" component={SettingsScreen} />
         </Drawer.Navigator>
     );
 }
-
-
