1296d38fd723df6d7415f707f37df634dffb2220
braian <brasanluc123@gmail.com>
2023-07-14 11:44:22 -0300

    saving last navigation changes

--- MiniProyecto/App.tsx
+++ MiniProyecto/App.tsx
@@ -1,17 +1,15 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
-// import { StackNavigation } from './src/Navigations/StackNavigation';
+import { StackNavigation } from './src/Navigations/StackNavigation';
 // import Carousel from './src/Components/Carousel';
-import { DrawerNavigation } from './src/Navigations/DrawerNavigation';
 
 export const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={{ flex: 1 }}>
-        {/* <StackNavigation /> */}
-        <DrawerNavigation />
+        <StackNavigation />
       </SafeAreaView>
     </>
     // <Carousel />
