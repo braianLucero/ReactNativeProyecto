858d98d4143439e8f4dc5e73fc921c5b7b913ac5
braian <brasanluc123@gmail.com>
2023-07-13 23:59:41 -0300

    adding fixes

--- MiniProyecto/src/Navigations/DrawerNavigation.tsx
+++ MiniProyecto/src/Navigations/DrawerNavigation.tsx
@@ -3,15 +3,27 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { StackNavigation } from './StackNavigation';
 import { HomeScreen } from './HomeScreen';
 import { SettingsScreen } from './SettingsScreen';
+import { ProfileScreen } from './ProfileScreen';
 
 const Drawer = createDrawerNavigator();
 
 export const DrawerNavigation = () => {
     return (
-        <Drawer.Navigator>
+        <Drawer.Navigator
+            initialRouteName="Home"
+            drawerStyle={{
+                backgroundColor: '#fff',
+                width: 240,
+            }}
+            drawerContentOptions={{
+                activeTintColor: '#e91e63',
+                itemStyle: { marginVertical: 10 },
+            }}
+        >
             <Drawer.Screen name="Home" component={HomeScreen} />
             <Drawer.Screen name="StackNavigation" component={StackNavigation} />
             <Drawer.Screen name="Settings" component={SettingsScreen} />
+            <Drawer.Screen name="Profile" component={ProfileScreen} />
         </Drawer.Navigator>
     );
 }
